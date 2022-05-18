81e76f311d673f9c65ffb1bdb02433dcc4e94ae7
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-05-18 12:48:47 +0300

    Лекция 3: вещественные числа

--- Lec3/example_c.py
+++ Lec3/example_c.py
@@ -1,11 +1,15 @@
 # Вещественные числа
-a_float = float(input()) # float("5.5") => 5.5 - пример явного приведения типов (из строки в целое число)
-b_float = float(input()) # float("3.5") => 3.5
+a_float = float(input())
+b_float = float(input())
 
 
 print(f"A value {a_float} and B value {b_float}")
 print("Type of A:", type(a_float))
 print(a_float, b_float)
+
+# Отрицательные числа
+print(-a_float)
+c_float = -a_float
 
 print("a - b = ", a_float - b_float)
 print("a + b = ", a_float + b_float)
@@ -15,3 +19,4 @@ print("a / b =", a_float / b_float) # Деление всегда веществ
 print("a // b=", a_float // b_float) # Целочисленное деление
 print("mod(a,b) = ", a_float % b_float) # Остаток от деления
 print("a ^ b = ", a_float ** b_float) # a_float - основнаия, b_float - показатель степени
+print("|-5.2| = ", abs(-5.2))
