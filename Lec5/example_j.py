96d10d2c431e2758fd68f6d3a5e970632a2ddfa2
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-05-25 11:07:49 +0300

    Лекция 5: цифл for

--- Lec5/example_j.py
+++ Lec5/example_j.py
@@ -1,7 +1,6 @@
 # Синтаксис цикла for
 # for expression:
 #     body
-
 # Простейший цикл for
 
 # i = 1
@@ -9,17 +8,27 @@
 #     print(i)
 #     i += 1
 
-for i in range(1, 10, 1): #[start, stop)
-    print("Value:", i)
+# for j in range(1, 10, 1): #[start, stop), step]
+#     print("Value:", j)
 
 # range(start, stop, step)
 # stop - это краевой параметр (т.е. итерации идут ДО НЕГО, не включая его самого)
 
-for i in range(1, 10): # range(start, stop[, step=1)
-    print("Another Value:", i)
+# for i in range(1, 10): # range(start, stop[, step=1)
+#     print("Another Value:", i)
 
-for i in range(10): # range(start=0, stop[, step=1)
+# При итерировании в сторону увеличения ОБЯЗАТЕЛЬНО:
+# * start < stop
+# * step > 0
+stopper = 15
+for i in range(stopper): # range(start=0, stop[, step=1)
     print("Third Value:", i)
+
+# При итерировании в сторону уменьшения ОБЯЗАТЕЛЬНО
+# * start > stop
+# * step < 0
+for j in range(0, -10, -2): # start=0, stop[-10, step = 1
+    print("Negative iterations:", j)
 
 # Важное ограничение - start/stop/step - всегда int
 
--- Lec5/example_k.py
+++ Lec5/example_k.py
@@ -1,6 +1,6 @@
 # Решение задачи M
-n = int(input()) # Допустим 2
+n = int(input())
 
-for i in range(-n, n + 1): # (-2, -1, 0, 1, 2)
-    sq = i ** 2
-    print(f"Квадрат числа {i} равен {sq}")
+for i in range(-n, n + 1):
+    squared = i ** 2
+    print(f"Квадрат числа {i} равен {squared}")
