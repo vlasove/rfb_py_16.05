74a9312ec6ce57c1a11ef507fd18d44f33deed20
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-06-06 11:33:40 +0300

    Лекция 10: Списочные выражения

--- Lec10/example_a.py
+++ Lec10/example_a.py
@@ -2,15 +2,17 @@
 
 numeric = None # Допустим это инициализирующее значение
 
-if 10%2 == 0:
+if 10 % 2 == 0:
     numeric = 2
 else:
     numeric = 5
 
 print("Numeric:", numeric)
 
+
+
 new_numeric = 2 if 10 % 2 == 0 else 5
-# value1 if expression else value2
-# возвращает value1 если expression  -> True
-# в противном случае возвращает value2
+#a = value1 if expression else value2
+# # возвращает value1 если expression  -> True
+# # в противном случае возвращает value2
 print("New numeric:", new_numeric)
--- Lec10/example_d.py
+++ Lec10/example_d.py
@@ -1,11 +1,12 @@
 # Пример. Считывание данных из входного потока
-# numerics_str = input()
-# list_numerics_str = numerics_str.split()
-# numerics = []
-# for elem in list_numerics_str:
-#     numerics.append(int(elem))
+numerics_str = input().strip() # "10 20 30 40 50 60"
+list_numerics_str = numerics_str.split() # ["10", "20", "30", "40", ...]
+print(list_numerics_str)
+numerics = []
+for elem in list_numerics_str:
+    numerics.append(int(elem)) # [10, 20, 30, 40, ...]
 
-# print(numerics)
+print(numerics)
 
 
 numerics = [int(elem) for elem in input().strip().split()] # ["10", "20", "30", "40", ...]
@@ -13,5 +14,6 @@ print(numerics) # [10, 20, 30, 40, ..]
 
 values = [1, 2, 3, 4, 88, 99, 100, 12]
 values = [elem + 1 for elem in values]
+print(values)
 
-print(", ".join([str(x) for x in values]))
+print(" : ".join([str(x) for x in values]))
