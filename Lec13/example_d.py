f9851b4bc33c612d7ae516b40f45493c1980bae6
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-06-10 13:07:39 +0300

    Лекция 13: Функци. Начало

---
+++ Lec13/example_d.py
@@ -0,0 +1,11 @@
+# Простейший возврат
+def add(a, b):
+    return a**2 + b**3
+
+# res = add(10, 10)
+# print("Result outside function:", res)
+
+#########################
+answer = add(2, -4) + add(1, 1) // 2 + add(2,3) ** 2
+print(f"Answer:{answer}")
+ 
