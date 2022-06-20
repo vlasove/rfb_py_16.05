1c75a6b2d003eab68e1a726390768ee1aba40ecd
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-06-20 11:44:30 +0300

    Лекция 19: Итоги

--- Lec19/example_j_json.py
+++ Lec19/example_j_json.py
@@ -1,5 +1,12 @@
 import json
 
+json_data = None
+with open("data.json", mode="r") as fh:
+    json_data = json.load(fh) # .load() - "вычитывание" / десериализация
+
+print('From json:', json_data)
+print(type(json_data))
+
 DATA = {
     "name" : "Bob",
     "last_name" : "Petrov",
@@ -8,9 +15,9 @@ DATA = {
 
 GENERAL_DATA = {
     "first" : DATA,
-    "second": DATA,
+    "second": [10, 20, 30, False, None, {"a":"a"}],
 }
 
 
-with open("data.json", "w") as fh:
-    json.dump(obj=GENERAL_DATA, fp=fh, indent=4)
+with open("data2.json", "w") as fh:
+    json.dump(obj=GENERAL_DATA, fp=fh, indent=4) # "запись" / сериализация
